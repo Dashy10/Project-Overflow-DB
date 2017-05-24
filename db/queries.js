c64a9023ff01bc512bfa4176895e1dffbc80f421
Dashy10 <dashiell.lumas@gmail.com>
2017-05-24 12:59:14 -0400

    full crud

--- db/queries.js
+++ db/queries.js
@@ -15,7 +15,46 @@ var db = pgp(connString);
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
 
+//////////////Create/////////////////
+////////////////////////////////////
+//////////////////////////////////
+createQuestion = (req,res,next) => {
+  db.none('INSERT INTO questions(question, topic_id)' +
+"values(${question}, ${topic_id})", req.body)
+.then(function (data) {
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Inserted one question'
+      });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+createAnswer = (req,res,next) => {
+  db.none('INSERT INTO answers(answer, question_id)' +
+  "values(${answer}, ${question_id})", req.body)
+  .then(function (data){
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Inserted one answer'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+}
+
+
+
 // //////////Read////////////
+/////////////////////////////
+////////////////////////////
 
 getAllDocumentation = (req,res,next) => {
   db.any('SELECT * FROM documentation')
@@ -31,6 +70,113 @@ getAllDocumentation = (req,res,next) => {
     return next(err);
   });
 };
+
+getAllQuestions = (req,res,next) => {
+  db.any('SELECT * FROM questions')
+  .then(function(data){
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Success in querying all questions'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
+
+getOneQuestion = (req,res,next) => {
+  var question_id = parseInt(req.params.question_id);
+  db.one('SELECT * FROM questions WHERE question_id=$1', question_id)
+  .then(function(data){
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'success in querying one question'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
+
+getAllAnswers = (req,res,next) => {
+  db.any('SELECT * FROM answers')
+  .then(function(data){
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Success in querying all answers'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
+
+getOneAnswer = (req,res,next) => {
+  var answer_id = parseInt(req.params.answer_id)
+  db.one('SELECT * FROM answers WHERE answer_id=$1', answer_id)
+  .then(function(data){
+    res.status(200)
+    .json({
+      status: 'Success',
+      data: data,
+      message: 'Success in querying one answer'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
+
+/////////Update////////////
+///////////////////////////
+//////////////////////////
+//////////////////////////
+updateQuestion = (req,res,next) => {
+  db.none('UPDATE questions SET question=$1, topic_id=$2',
+  [req.body.question, req.body.topic_id])
+  .then(function(){
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'updated question'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+}
+
+updateAnswer = (req,res,next) => {
+  db.none('UPDATE answers SET answer=$1, question_id=$2',
+  [req.body.answer, req.body.question_id])
+  .then(function(){
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'updated answer'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+}
+
+
+/////////Delete////////////
+///////////////////////////
+//////////////////////////
+//////////////////////////
+
+deleteAnswer = (req,res,next) => {
+  var answer_id = parseInt(req.params.answer_id);
+  db.result('DELETE FROM answers WHERE answer_id=$1', answer_id)
+}
 //
 // getAllQuestionsWithAnswersBySubject = (req,res,next) => {
 //
@@ -86,7 +232,16 @@ getAllDocumentation = (req,res,next) => {
 
 module.exports = {
   getAllDocumentation: getAllDocumentation,
-//  deleteAnswer: deleteAnswer,
+  getAllQuestions: getAllQuestions,
+  getOneQuestion: getOneQuestion,
+  getAllAnswers: getAllAnswers,
+  getOneAnswer: getOneAnswer,
+  deleteAnswer: deleteAnswer,
+  createQuestion: createQuestion,
+  createAnswer: createAnswer,
+  updateQuestion: updateQuestion,
+  updateAnswer: updateAnswer,
+
 };
 
 
--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,15 @@ router.get('/', function(req, res, next) {
 // all the documentation stored on this route
 router.get('/documentation', db.getAllDocumentation);
 
-// // QUESTIONS
-// // all the questions are stored on this route
-// router.get('/questions', db.getAllQuestions);
+// QUESTIONS
+// all the questions are stored on this route
+router.get('/questions', db.getAllQuestions);
 // // post a question that will be stored in this route
-// router.post('/questions',db.createQuestion);
+router.post('/questions',db.createQuestion);
 // // get one question based on its id
-// router.get('/questions/:question_id', db.getOneQuestion);
+router.get('/questions/:question_id', db.getOneQuestion);
 // // update one question based on its id
-// router.patch('/questions/:question_id', db.updateQuestion);
+router.patch('/questions/:question_id', db.updateQuestion);
 //
 //
 // // QUESTIONS + ANSWERS ORGANIZED BY SUBJECT
@@ -36,13 +36,14 @@ router.get('/documentation', db.getAllDocumentation);
 //
 // // ANSWERS
 // // all the answers in total are stored in this route
-// router.get('/answers/', db.getAllAnswers);
+router.get('/answers/', db.getAllAnswers);
+router.get('/answers/:answer_id', db.getOneAnswer);
 // // post an answer that will be stored in this route
-// router.post('/answers/', db.createAnswer);
+router.post('/answers/', db.createAnswer);
 // // delete one answer based on its id
-// router.delete('/answers/:answer_id', db.deleteAnswer);
+router.delete('/answers/:answer_id', db.deleteAnswer);
 // // update one answer based on its id
-// router.patch('/answers/:answer_id', db.updateAnswer);
+router.patch('/answers/:answer_id', db.updateAnswer);
 
 
 
