48dd261ea7aa326ba50298900b434fbcc6e225cb
Dashy10 <dashiell.lumas@gmail.com>
2017-05-28 12:37:51 -0400

    changed front page for API

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var db = require('../db/queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ProjectOverflow API Documentation' });
 });
 
 
